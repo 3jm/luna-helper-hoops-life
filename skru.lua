d0906af527c8c8438a27ec075d9c03db7b693a0b
Unknown <68416923+3jm@users.noreply.github.com>
2022-06-24 00:47:30 -0400

    Update skru.lua

--- skru.lua
+++ skru.lua
@@ -1,4 +1,12 @@
 if game.PlaceId == 9742262009 then
+    local StarterGui = game:GetService("StarterGui")
+    local Notifications = {
+        ["Title"] = "Skru",
+        ["Text"] = "Skru is now loaded! @NBA_Youngboy owns everyone in your game now.",
+        ["Duration"] = 5,
+        ["Icon"] = "rbxassetid://9798398854"
+    }
+    StarterGui:SetCore("SendNotification", Notifications)
     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
     local colors = {
         SchemeColor = Color3.fromRGB(249,0,77),
@@ -7,27 +15,8 @@ if game.PlaceId == 9742262009 then
         TextColor = Color3.fromRGB(255,255,255),
         ElementColor = Color3.fromRGB(20, 20, 20)
     }
-    local Window = Library.CreateLib("Skru: By @NBA_Grownboy", colors)
-
-    -- MAIN SECTION --
-    local Main = Window:NewTab("Main")
-    local MainSection = Main:NewSection("Main")
-
-    -- SHOOTING SECTION --
-    local Shooting = Window:NewTab("Shooting")
-    local ShootingSection = Shooting:NewSection("Shooting")
+    local Window = Library.CreateLib("Luna Helper | [Hoops Life] | Blind#2665", colors)
     
-
-    -- PHYSICS ENGINE SECTION --
-    local PhysicsEngine = Window:NewTab("Physics Engine")
-    local PhysicsEngineSection = PhysicsEngine:NewSection("Physics Engine")
-    PhysicsEngineSection:NewSlider("Walkspeed", "'1' is default walking speed.", 5000, 84, function(v) -- 500 (MaxValue) | 0 (MinValue)
-        game:GetService("Players").LocalPlayer.Attributes.Speed.Value = v
-    end)
-    PhysicsEngineSection:NewSlider("Jumppower", "'1' is default jump power", 500, 0, function(v) -- 500 (MaxValue) | 0 (MinValue)
-        game.Players.LocalPlayer.Character.Humanoid.JumpPower = v
-    end)
-        
 
     -- VISUALS SECTION --
     local Visuals = Window:NewTab("Visuals")
@@ -36,7 +25,7 @@ if game.PlaceId == 9742262009 then
         game:GetService("Players").LocalPlayer.CameraMaxZoomDistance = 5000
         game:GetService("Players").LocalPlayer.CameraMinZoomDistance = 0.5
     end)
-    
+
     
 
     -- LIGHTING SECTION --
@@ -52,13 +41,21 @@ if game.PlaceId == 9742262009 then
     LightingSection:NewColorPicker("Atmosphere Color", "Changes Atmosphere Color", Color3.fromRGB(0,0,0), function(color)
         game:GetService("Lighting").ColorCorrection.TintColor = color
     end)
-
-
-    -- SETTINGS SECTION --
-    local Settings = Window:NewTab("Settings")
-    local SettingsSection = Settings:NewSection("Settings")
-    SettingsSection:NewKeybind("Toggle UI", "Open & Close menu", Enum.KeyCode.Insert, function()
-        Library:ToggleUI()
+    LightingSection:NewSlider("Saturation", "funny", 100, -100, function(v)
+        game:GetService("Lighting").ColorCorrection.Saturation = v
     end)
-    
+    LightingSection:NewButton("Reset", "Resets Saturation to default", function()
+        game:GetService("Lighting").ColorCorrection.Saturation = 0
+    end)
+
+
+elseif game.PlaceId ~= 9742262009 then
+    local StarterGui = game:GetService("StarterGui")
+    local Notifications = {
+        ["Title"] = "Skru [ERROR]",
+        ["Text"] = "Skru can only be loaded when you are in the park!",
+        ["Duration"] = 5,
+        ["Icon"] = "rbxassetid://9798398854"
+    }
+    StarterGui:SetCore("SendNotification", Notifications)
 end
