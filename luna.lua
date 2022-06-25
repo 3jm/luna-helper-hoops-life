6f60fb075b4619104f653fee03f699428de87fb7
Unknown <68416923+3jm@users.noreply.github.com>
2022-06-25 18:18:43 -0400

    added more teleport features

--- luna.lua
+++ luna.lua
@@ -1,8 +1,8 @@
 if game.PlaceId == 10008450542 or 10008443595 then
     local StarterGui = game:GetService("StarterGui")
     local Notifications = {
-        ["Title"] = "Skru",
-        ["Text"] = "Skru is now loaded! @NBA_Youngboy owns everyone in your game now.",
+        ["Title"] = "Luna",
+        ["Text"] = "Luna is now loaded! @NBA_Youngboy owns everyone in your game now.",
         ["Duration"] = 5,
         ["Icon"] = "rbxassetid://9798398854"
     }
@@ -46,16 +46,133 @@ if game.PlaceId == 10008450542 or 10008443595 then
 
     -- Teleport Section -- 
     local Teleport = Window:NewTab("Teleport")
-    local TeleportSection = Teleport:NewSection("Teleport")
+    local TeleportSection = Teleport:NewSection("Teleport (PLAZA ONLY)")
     TeleportSection:NewButton("Park", "Teleports you to park enterence (PLAZA ONLY)", function()
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(180,3.37,-88)
+        if game.PlaceId == 10008443595 then
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(180,3.37,-88)
+        else
+            local StarterGui = game:GetService("StarterGui")
+            local Notifications = {
+                ["Title"] = "Luna Helper [ERROR]",
+                ["Text"] = "This only works when you are in the plaza!",
+                ["Duration"] = 5,
+                ["Icon"] = "rbxassetid://9798398854"
+            }
+    StarterGui:SetCore("SendNotification", Notifications)
+        end
+    end)
+    TeleportSection:NewButton("Mall", "Teleports you to the mall (PLAZA ONLY)", function()
+        if game.PlaceId == 10008443595 then
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-96.99,7.15,-575.83)
+        else
+            local StarterGui = game:GetService("StarterGui")
+            local Notifications = {
+                ["Title"] = "Luna Helper [ERROR]",
+                ["Text"] = "This only works when you are in the plaza!",
+                ["Duration"] = 5,
+                ["Icon"] = "rbxassetid://9798398854"
+            }
+    StarterGui:SetCore("SendNotification", Notifications)
+        end
+    end)
+    TeleportSection:NewButton("Haircut Shop", "Teleports you to the haircut shop (PLAZA ONLY)", function()
+        if game.PlaceId == 10008443595 then
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10.64,3.5,-250.21)
+        else
+            local StarterGui = game:GetService("StarterGui")
+            local Notifications = {
+                ["Title"] = "Luna Helper [ERROR]",
+                ["Text"] = "This only works when you are in the plaza!",
+                ["Duration"] = 5,
+                ["Icon"] = "rbxassetid://9798398854"
+            }
+    StarterGui:SetCore("SendNotification", Notifications)
+        end
+    end)
+    TeleportSection:NewButton("Clothing Shop", "Teleports you to the clothing shop (PLAZA ONLY)", function()
+        if game.PlaceId == 10008443595 then
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-50.03,3.5,-258.06)
+        else
+            local StarterGui = game:GetService("StarterGui")
+            local Notifications = {
+                ["Title"] = "Luna Helper [ERROR]",
+                ["Text"] = "This only works when you are in the plaza!",
+                ["Duration"] = 5,
+                ["Icon"] = "rbxassetid://9798398854"
+            }
+    StarterGui:SetCore("SendNotification", Notifications)
+        end
+    end)
+    TeleportSection:NewButton("Gym", "Teleports you to the gym (PLAZA ONLY)", function()
+        if game.PlaceId == 10008443595 then
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-47.74,3.5,-11.05)
+        else
+            local StarterGui = game:GetService("StarterGui")
+            local Notifications = {
+                ["Title"] = "Luna Helper [ERROR]",
+                ["Text"] = "This only works when you are in the plaza!",
+                ["Duration"] = 5,
+                ["Icon"] = "rbxassetid://9798398854"
+            }
+    StarterGui:SetCore("SendNotification", Notifications)
+        end
+    end)
+    TeleportSection:NewButton("MatchMaker", "Teleports you to the matchmaker (PLAZA ONLY)", function()
+        if game.PlaceId == 10008443595 then
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(86.79,3.5,29.75)
+        else
+            local StarterGui = game:GetService("StarterGui")
+            local Notifications = {
+                ["Title"] = "Luna Helper [ERROR]",
+                ["Text"] = "This only works when you are in the plaza!",
+                ["Duration"] = 5,
+                ["Icon"] = "rbxassetid://9798398854"
+            }
+    StarterGui:SetCore("SendNotification", Notifications)
+        end
+    end)
+    TeleportSection:NewLabel("Teleport (COURT ONLY)")
+    TeleportSection:NewButton("Back To Plaza", "Teleports you to plaza return", function()
+        if game.PlaceId == 10008450542 then
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(399.27,23.35,-214.65)
+        else
+            local StarterGui = game:GetService("StarterGui")
+            local Notifications = {
+                ["Title"] = "Luna Helper [ERROR]",
+                ["Text"] = "This only works when you are on the court!",
+                ["Duration"] = 5,
+                ["Icon"] = "rbxassetid://9798398854"
+            }
+    StarterGui:SetCore("SendNotification", Notifications)
+        end
+    end)
+    TeleportSection:NewLabel("Teleport (GLOBAL)")
+    TeleportSection:NewTextBox("Teleport To", "Player Name", function(txt)
+        local p1 = game.Players.LocalPlayer.Character.HumanoidRootPart
+        local p2 = txt
+        p1.CFrame = game.Players[p2].Character.HumanoidRootPart.CFrame 
+        --[[
+            no check to see if they're in the game or not yet. If you dont get teleported, their player is not in the game
+            or you typed their name wrong.
+        ]]--
+    end)
+
+    -- Credits Section --
+    local Credits = Window:NewTab("Credits")
+    local CreditsSection = Credits:NewSection("Dev: NBA_Grownboy")
+
+    -- Settings Section --
+    local Settings = Window:NewTab("Settings")
+    local SettingsSection = Settings:NewSection("Settings")
+    SettingsSection:NewKeybind("Open/Close", "Open/Close menu button", Enum.KeyCode.Insert, function()
+        Library:ToggleUI()
     end)
 
 
 elseif game.PlaceId ~= 10008450542 or 10008443595 then
     local StarterGui = game:GetService("StarterGui")
     local Notifications = {
-        ["Title"] = "Skru [ERROR]",
+        ["Title"] = "Luna Helper [ERROR]",
         ["Text"] = "Skru can only be loaded when you are in the park!",
         ["Duration"] = 5,
         ["Icon"] = "rbxassetid://9798398854"
